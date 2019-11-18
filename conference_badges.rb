7d533ac30d930d74172b9eba2d1c7154b87da7f2
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 21:37:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  badges
+end
