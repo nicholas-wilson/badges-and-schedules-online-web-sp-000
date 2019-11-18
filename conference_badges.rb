8c7eea1b0b9eb60aad1d1ed6e8f8ac3de855a4c2
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 22:24:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(names)
+  rooms = []
+  names.each_index do |name|
+    rooms << "Hello, #{names[name]}! You'll be assigned to room #{name + 1}!"
+  end
+  rooms
+end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  assigned_rooms = assign_rooms(names)
+  names.each_index do |current|
+    puts badges[current]
+    puts assigned_rooms[current]
+  end
+end
